a271b709951023179b0488522bb97efcf448e224
Smile <147833000+Abdurakhmanov20@users.noreply.github.com>
2023-10-25 12:01:34 +0300

    Add files via upload

---
+++ 01-variables/homework/index.js
@@ -0,0 +1,26 @@
+6 > 4 && 10 < 5 
+//false - ложь
+(30 + 4) > (7 ** 2) 
+ // false - ложь
+
+null && true null
+
+!('hi' + 'hi') && true 
+// false - ложь
+
+!('hi' + 'hi') && false 
+// false ложь 
+
+'Привет' + 'Друг' == !null 
+// Это оператор равенства
+'Привет' + 'Друг' === !null 
+// Это оператор строгово равенства
+
+const ten = 10; 
+const hundred = 100;
+const thousand = 1000;
+
+console.log((ten * ten + hundred) * hundred >= thousand * ten ) 
+// true - истинна 
+
+
