c629b2a6a94e8950f38af2051147f5969fe24482
Akhmed <147833000+Abdurakhmanov20@users.noreply.github.com>
2023-11-15 21:58:14 +0300

    Add files via upload

---
+++ 06-Callback/lesson/lesson.js
@@ -0,0 +1,18 @@
+function appendValue(value) {
+  document.getElementById("result").value += value;
+}
+
+function clearResult() {
+  document.getElementById("result").value = '';
+}
+
+function deleteValue() {
+  let result = document.getElementById("result").value;
+  document.getElementById("result").value = result.slice(0, -1);
+}
+
+function calculateResult() {
+  let result = document.getElementById("result").value;
+  let calculation = eval(result);
+  document.getElementById("result").value = calculation;
+}
